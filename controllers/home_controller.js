243151855c13397e7769fc852aa0fdffd2a1553f
Nanda Kishore Reddy Desani <nandakishoredesani@gmail.com>
2020-11-30 22:20:21 -0500

    fourth  commit:setting up layout viewfile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 module.exports.home=function(req,res)
 {
-    return res.end('<h1> express is setup for codeial</h1>');
+    return res.render('home',{
+        title:"home"
+    });
 }
 module.exports.contact=function(req,res)
 {
